3fd2a58a6f4e6970c4f2e09c167cebbcb99990c4
Mathieusc <mathieu.schweitzer@hotmail.fr>
2021-12-07 16:28:10 +0100

    clean code

--- bruteforce.py
+++ bruteforce.py
@@ -1,3 +1,10 @@
+"""
+bruteforce.py tries all combinations possible of actions that cost less
+than 500€ and find the best profit in euros.
+The execution time for this method is also calculated.
+"""
+
+
 from itertools import combinations
 from timeit import timeit
 from time import time
@@ -25,35 +32,60 @@ actions = [
     (4, 12),
     (10, 14),
     (24, 21),
-    (114, 18)
+    (114, 18),
 ]
 
 # Create a list of action cost.
 action_cost_list = [action[0] for action in actions]
-# Associate each action cost to their profit in euros in a dictionary.
+# Associate each action cost to their profit (calculated in euros) in a dictionary.
 action_profit = {action[0]: (action[0] * action[1]) / 100 for action in actions}
 
 
-
 def bruteforce(action, profit):
+    """
+    Try all combinations of actions below 500€.
+    Return:
+        tuple() - The single best combination of profit in euros and their actions.
+    """
     results = []
-    for i in range(1, len(action)):
+    for i in range(1, len(action) + 1):
         action_combinations = combinations(action, i)
         actions_profit_list = []
         for action_combination in action_combinations:
             if sum(action_combination) <= 500:
-                actions_profit_list.append((action_combination, sum([profit[action] for action in action_combination])))
+                actions_profit_list.append(
+                    (
+                        action_combination,
+                        sum([profit[action] for action in action_combination]),
+                    )
+                )
         results += actions_profit_list
 
-    return max(results, key = lambda x: x[1])
+    return max(results, key=lambda x: x[1])
 
 
 def execution_time(start, end):
+    """
+    Display the executime of the bruteforce method.
+    """
     execution_time = end_time - start_time
     display_execution_time = f"The bruteforce method took {execution_time:.2f} seconds."
     print(display_execution_time)
 
+
+def display_best_profit(bruteforce_result):
+    """
+    Display the best combination of actions in euros (total + each action)
+    and the best profit in euros.
+    """
+    display_best_profit = f"Combination of actions in euros : {find_best_profit[0]}\n\t\
+Profit : {find_best_profit[1]:.2f}€\n\t\
+Total cost : {sum(find_best_profit[0])}€"
+    print(display_best_profit)
+
+
 find_best_profit = bruteforce(action_cost_list, action_profit)
+display_best_profit(find_best_profit)
 
 end_time = time()
 execution_time(start_time, end_time)
